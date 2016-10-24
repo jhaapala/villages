bda7ed022d93457ae2a345ba7008ce6180a91659
Carlton Hanna <nullpointer0x00@gmail.com>
2016-10-24 18:24:03 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get -y install python-dev
-sudo apt-get install python-pip
+sudo apt-get -y install python-pip
+sudo apt-get install python3-lxml
+sudo pip install lxml
+sudo pip install requests
